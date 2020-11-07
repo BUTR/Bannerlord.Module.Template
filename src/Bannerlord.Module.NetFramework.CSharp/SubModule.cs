ecff30cc5a0b891eade84903acab7871764e6e6c
Vitaly Mikhailov <personal@aragas.org>
2020-11-07 19:31:16 +0300

    init

---
+++ src/Bannerlord.Module.NetFramework.CSharp/SubModule.cs
@@ -0,0 +1,25 @@
+﻿using TaleWorlds.MountAndBlade;
+
+namespace BLNamespace
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
